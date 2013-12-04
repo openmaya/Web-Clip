869c7fbf9aaf6495b07cb14a2f94b3f4f77646bd
openmaya <openmaya@gmail.com>
2013-12-04 13:32:25 +0900

    directory name

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-	$CP_BASEDIR="/home/fibonacci/public_html/cp/";
+	$CP_BASEDIR=str_replace(basename(__FILE__), "", realpath(__FILE__));
 	$CONTROL_PATH=$CP_BASEDIR."controls/";
 	$VIEW_PATH=$CP_BASEDIR."views/";
 	$MODEL_PATH=$CP_BASEDIR."models/";
 	$CP_BASEURL="http://openmaya.com/cp/";
-
 ?>
